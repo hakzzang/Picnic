0ce8b153f86982ae1bf877429b4f31fd001582e6
hakzzang <mbh930312@naver.com>
2019-09-22 12:26:11 +0900

    채팅 보낼 시, 구독 및 노티피케이션 로직 추가

--- app/src/main/java/hbs/com/picnic/utils/FcmService.kt
+++ app/src/main/java/hbs/com/picnic/utils/FcmService.kt
@@ -1,26 +1,17 @@
 package hbs.com.picnic.utils
 
-import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-
-
+import hbs.com.picnic.R
 
 class FcmService : FirebaseMessagingService(){
     private val notificationUtil = NotificationUtil()
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d("hello", "From: " + remoteMessage.from!!)
-
-        if (remoteMessage.data.size > 0) {
-            Log.d("hello", "Message data payload: " + remoteMessage.data)
-
+        if(remoteMessage.data["uid"] == FirebaseAuth.getInstance().currentUser?.uid){
+            //uid 비교로 로직처리
         }
 
-        if (remoteMessage.notification != null) {
-            Log.d("hello", "Message Notification Body: " + remoteMessage.notification!!.body!!)
-        }
-
-        notificationUtil.send(this)
-
+        notificationUtil.send(this, remoteMessage, R.drawable.ic_foodtruck_line)
     }
 }
