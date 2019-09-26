57f889795574f90a5b42c657e5d8080813fd17e9
ggujangi <asmwj3@naver.com>
2019-09-27 01:20:05 +0900

    Recommend 타입, 데이터 모델 추가

---
+++ app/src/main/java/hbs/com/picnic/data/model/ParkInfo.kt
@@ -0,0 +1,21 @@
+package hbs.com.picnic.data.model
+
+data class ParkInfo (
+    val title:String,
+    val content:String,
+    val land:String,
+    val date:String,
+    val facilities:String,
+    val plants:String,
+    val mapImg:String,
+    val way:String,
+    val notice:String,
+    val img:String,
+    val addr:String,
+    val tel:String,
+    val x:String,
+    val y:String,
+    val x1:String,
+    val y1:String,
+    val url:String
+)
